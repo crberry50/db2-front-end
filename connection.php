8eb70bdb3a2b56d13fcb6d9ec3cad1cc1c696382
Andrew Laidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2022-04-06 09:43:39 +0100

    Updating connection logic to work with newer versions of PHP

--- connection.php
+++ connection.php
@@ -4,13 +4,22 @@
 // Any dashes (-) should be converted to underscores (_)
 // We pull this in from an environment varaible if set, otherwise using the default
 if ($_ENV['DB2_API_SERVICE_NAME']) {
-    $apiInstanceName = $_ENV['DB2_API_SERVICE_NAME'];
+    $apiName = strtoupper($_ENV['DB2_API_SERVICE_NAME']);
+    $apiInstanceName = str_replace("-", "_", $apiName);
 } else {
     $apiInstanceName = 'DB2_READER_API';
 }
 
+// Create the relevant environment variables from the Instance Name
+$hostenv = $apiInstanceName . '_SERVICE_HOST';
+$portenv = $apiInstanceName . '_SERVICE_PORT';
+
+// Get details from the environment variables
+$host = getenv($hostenv);
+$port = getenv($portenv);
+
 // Here we set the endpoint of our API instance
-$apiBaseUri = 'http://' . $_ENV[$apiInstanceName.'_SERVICE_HOST'] . ':' . $_ENV[$apiInstanceName.'_SERVICE_PORT'];
+$apiBaseUri = 'http://' . $host . ':' . $port;
 
 // This varaiable $apiBaseUri can then be used in all the pages
 
